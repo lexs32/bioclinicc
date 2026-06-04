d7a72eb9866cb1d3b42b310d9046cb3dc70a0788
lexs32 <hadjmed2030@gmail.com>
2026-06-04 20:09:38 +0100

    Add debug logs to Vercel entrypoint

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import { handle } from 'hono/vercel';
 import { app } from '../build/server/index.js';
 
-export default handle(app);
+console.log("API entrypoint loaded, app is:", !!app);
+
+const handler = handle(app);
+
+export default (req, res) => {
+  console.log("Vercel function invoked for path:", req.url);
+  return handler(req, res);
+};
